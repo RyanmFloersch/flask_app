197cf6ff579da8e70e3317a93e77f69f50f12117
Ryan <ryanmfloersch@gmail.com>
2023-07-10 16:30:12 -0400

    Added opencv elements to camera.py so when it is retrieving frames it will add a box around detected objects. The only problem is how much lag this causes the video.

--- flask-server/camera.py
+++ flask-server/camera.py
@@ -1,8 +1,13 @@
 import cv2
+import cvlib as cv
+# Drawbox draws a box around detected objects
+from cvlib.object_detection import draw_bbox
+
 
 class VideoCamera(object):
     # start the video
     def __init__(self) -> None:
+
         self.video = cv2.VideoCapture(0)
 
     # end the video
@@ -11,7 +16,21 @@ class VideoCamera(object):
     
     # retrieve frames and convert them into jpg and to bytes.
     def get_frame(self):
+        # read from VideoCameras instance video the frame.
         ret, frame = self.video.read()
-        ret, jpeg = cv2.imencode('.jpg', frame)
+            
+                
+        # detect objects in the frame.
+        # bbox sets up border around object.
+        # label is the label for the object
+        # conf is decimal numbers for object identification
+        bbox, label, conf = cv.detect_common_objects(frame)
+
+        # Applies a box to the frame where the detected object is
+        output_image = draw_bbox(frame, bbox, label, conf)
+
+        # convert the output_image format to jpg
+        ret, jpeg = cv2.imencode('.jpg', output_image)
+        # convert jpeg frame to bytes for easier transfer
         return jpeg.tobytes()
     
